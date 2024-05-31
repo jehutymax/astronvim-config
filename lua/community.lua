27a7c67cdca37cc93d92b801be4375735331c005
Rafael Campos <rcampos@mailworks.org>
2024-05-31 12:58:11 -0700

    Starting config

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  -- import/override with your plugins folder
+}
